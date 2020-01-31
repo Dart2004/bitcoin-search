481e54df829674f3e86f2260e6b3bcf1ad4d4058
Dart2004 <59048496+Dart2004@users.noreply.github.com>
2020-01-31 12:48:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Bitcoins</title>
+  </head>
+  <body>
+     <?php
+      echo "<div style='text-align:center'>";
+      echo "<h1>MAKE ME RICH</h1>";
+      echo "<form action='index2.php' method='post'>";
+      echo "<input id='name' name='name' placeholder='Type in your name'>";?>
+  </body>
+</html>
